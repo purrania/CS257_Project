e9ae917c53dd786fd442611a219c9aba7c8252d9
June Lee <junelee1@myth61.stanford.edu>
2026-02-21 22:45:55 -0800

    added backtrack func and updated decide but need to properly integrate it in to solve

--- solver.py
+++ solver.py
@@ -43,18 +43,26 @@ def propagate(clauses, M):
     update_msg(M, changed, "prop")
     return M, changed
 
-def backtrack():
-    pass
+def backtrack(M, decision_points):
+    if len(decision_points) == 0:
+        return M
+    
+    decision_point = decision_points.pop() # again cant tell if this is off by one error NEED TO TEST
+
+    M = M[:decision_point] + [-M[decision_point]]
+    return M
 
 def fail():
+    # tbh idek if we need this function but i'll change solve first to see if we need it
     pass
 
-def decide(clauses, M):
+def decide(clauses, M, decision_points):
     changed = False
     for literal in clauses.literals:
         if literal not in M and -literal not in M:
             M =  M + [literal]
             changed = True
+            decision_points.append(len(M)) # basically save the places where the big dots in the slide would be - might have off by one error so gotta test logic
             break
     update_msg(M, changed, "decide")
     return M, changed
@@ -75,6 +83,7 @@ def solve(test_case):
     clauses = test_case.clauses
     result = True
     M = []
+    decision_points = []
     i = 0 
     if test_case.result == SAT:
         # NOTE: I have not added the backtrack or fail cases
@@ -86,7 +95,7 @@ def solve(test_case):
             M, changed = propagate(clauses, M)
             if changed:
                 continue
-            M, changed = decide(clauses, M)
+            M, changed = decide(clauses, M, decision_points)
             i += 1
     solution = SolverResult(result, assignment=M)
     print("\nSolution is", solution.assignment, "\n")
